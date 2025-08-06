db2a9ac3d9983a8ababe0ab476c85067d4e1d19c
tf63 <74246282+tf63@users.noreply.github.com>
2025-08-06 17:45:20 +0900

    feat: add commands for verification

---
+++ cmd/context-pattern/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/tf63/go-gorm-tx-sample/internal/context-pattern/application"
+	"github.com/tf63/go-gorm-tx-sample/internal/context-pattern/infrastracture"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// DB setup
+	db, err := gorm.Open(mysql.Open("root:rootpassword@tcp(127.0.0.1:3306)/sample"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// DI
+	txManager := infrastracture.NewTransactionManager(db)
+	ar := infrastracture.NewAccountRepository(db)
+	au := application.NewAccountUsecase(ar, txManager)
+
+	// Request
+	ctx := context.Background()
+
+	fromID := 2
+	toID := 3
+
+	account_1, _ := ar.FindByID(ctx, fromID)
+	account_2, _ := ar.FindByID(ctx, toID)
+
+	fmt.Printf("Account %d Balance: %d\n", fromID, account_1.Balance)
+	fmt.Printf("Account %d Balance: %d\n", toID, account_2.Balance)
+
+	err = au.Transfer(ctx, fromID, toID, 100)
+	if err != nil {
+		fmt.Println("Transfer failed:", err)
+		return
+	}
+
+	// Output
+	fmt.Println("Transfer completed successfully")
+
+	account_1, _ = ar.FindByID(ctx, fromID)
+	account_2, _ = ar.FindByID(ctx, toID)
+
+	fmt.Printf("Account %d Balance: %d\n", fromID, account_1.Balance)
+	fmt.Printf("Account %d Balance: %d\n", toID, account_2.Balance)
+}
